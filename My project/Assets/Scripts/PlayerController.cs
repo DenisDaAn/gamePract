9bcda378e50484ef60f2dbec54d2733e4cea0df0
Admin <denhita@yandex.ru>
2022-09-08 21:20:39 +0300

    Add menu & management

--- My project/Assets/Scripts/PlayerController.cs
+++ My project/Assets/Scripts/PlayerController.cs
@@ -4,21 +4,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    //public CharacterController controller;
-    //public Vector3 dir;
+    public static bool tap, swipeLeft, swipeRight, swipeUp, swipeDown;
+    private bool isDraging = false;
+    private Vector2 startTouch, swipeDelta;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        //controller = GetComponent<CharacterController>();
 
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.position += new Vector3(0, 0, 1);
-        //controller.Move(dir * Time.fixedDeltaTime);
+        transform.position += new Vector3(0, 0, 1 * Time.deltaTime);
+        while(Input.GetKeyDown(KeyCode.RightArrow) == true)
+        {
+            transform.position += Vector3.right * Time.deltaTime;
+        }
+        while (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * Time.deltaTime;
+        }
     }
 }
